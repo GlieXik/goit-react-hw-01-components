2562777339f31241afffbe01dadbf5886655ecf2
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-08-06 11:39:31 +0300

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Profile } from "./Profile/Profile";
+import user from "../json/user"
+import {Statistics} from "./Statistics/Statistics"
+import { ThemeProvider } from "styled-components";
+import {theme} from "../theme"
+import friends from "../json/friends"
+import {FriendList} from "./FriendList/FriendList"
+import transactions from "../json/transactions"
+import {TransactionHistory} from "./TransactionHistory/TransactionHistory"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  <ThemeProvider theme={theme}>
+      <Profile username={user.username} tag={user.tag} location= {user.location} avatar={user.avatar} stats={user.stats}></Profile>
+      <Statistics/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+  </ThemeProvider>
   );
 };
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const ProfileContainer = styled.div`
+margin:20px;
+border:1px solid #000;
+width: 300px;
+
+border-radius: 15px;
+padding: 15px;
+background-color: #fff;
+position: relative;
+height: 240px;
+`
+export const Avatar = styled.img`
+display: block;
+margin-left: auto;
+margin-right: auto;
+height: 90px;
+width: 90px;
+border-radius: 50%;
+`
+
+export const UserName = styled.p`
+text-align: center;
+font-size:20px;
+margin:10px 0 5px 0;
+`
+export const UserTag = styled.p`
+text-align: center;
+font-size:15px;
+margin:0;
+color: rgb(95, 95, 95);
+`
+export const UserLocation = styled.p`
+text-align: center;
+font-size:15px;
+margin:10px 0 5px 0;
+`
